82480a4899cc8dcca944462fdfdff1f1295312cb
Robert Ridal <ridal.robert@gmail.com>
2020-05-28 21:43:36 +0300

    Removed reddit using praw

--- main_bot.py
+++ main_bot.py
@@ -2,6 +2,8 @@ import discord
 import os
 from discord.ext import commands
 
+import logging
+
 from dotenv import load_dotenv
 from pathlib import Path
 # path to .env
@@ -11,6 +13,8 @@ load_dotenv(dotenv_path=env_path, verbose=True)
 command_prefix = 'chika'
 chika = commands.Bot(command_prefix=command_prefix)
 TOKEN = os.getenv("TOKEN")
+
+logging.basicConfig(level=logging.DEBUG)
 
 # get all the cogs
 for filename in os.listdir('./cogs'):
