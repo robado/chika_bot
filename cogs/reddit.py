dfa93f46fe9146bfd5a82376aba3823c713700f1
Robert Ridal <ridal.robert@gmail.com>
2020-05-31 18:35:21 +0300

    Small fixes for reddit posts

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -36,7 +36,7 @@ class RedditPosts(commands.Cog):
                              password=os.getenv("PASSWORD"),
                              user_agent=os.getenv("USER_AGENT"),
                              username=os.environ.get("REDDIT_USERNAME"))
-        channel = self.chika.get_channel(int(os.environ.get("DISCORD_BOT_SPAM_CHANNEL_ID")))  # specific channel
+        channel = self.chika.get_channel(int(os.environ.get("DISCORD_CHANNEL_ID")))  # specific channel
         colour = discord.colour.Color.red()
         # Get the post
         for posts in reddit.subreddit(os.environ.get("SUBREDDIT")).stream.submissions(pause_after=0):
@@ -44,15 +44,16 @@ class RedditPosts(commands.Cog):
                 await asyncio.sleep(60)
             elif posts.created_utc > current_date_in_seconds:
                 if posts.is_self == True and posts.is_reddit_media_domain == False:
+                    print("A new text post was posted with id: {}".format(posts.id))
                     # if posts.over_18 == True:
                     #     print("Content is over_18")
-                    embed = discord.Embed(title='New text post on {}'.format(posts.subreddit_name_prefixed),
+                    embed = discord.Embed(name='{}'.format(posts.title),
                                           url=posts.url,
                                           # description='{}'.format(posts.selftext),
                                           description='{}'.format((posts.selftext[:2045] + '...') if len(
                                               posts.selftext) > 2048 else posts.selftext),
                                           colour=colour)
-                    embed.set_author(name='{}'.format(posts.title),
+                    embed.set_author(name='New text post on {}'.format(posts.subreddit_name_prefixed),
                                      url='{}'.format(posts.url))
                     embed.add_field(name='Author',
                                     value='{}'.format(posts.author),
@@ -64,11 +65,12 @@ class RedditPosts(commands.Cog):
                     await channel.send(embed=embed)
                 if posts.is_self == False and posts.is_reddit_media_domain == True:
                     if posts.over_18 == True:
-                        embed = discord.Embed(title='New image post on {}'.format(posts.subreddit_name_prefixed),
-                                              url=posts.url,
-                                              colour=colour)
-                        embed.set_author(name='{}'.format(posts.title),
-                                         url='{}{}'.format(os.environ.get("REDDIT_BASE_URL"), posts.permalink))
+                        print("A new K-18 image post was posted with id: {}".format(posts.id))
+                        embed = discord.Embed(title='{}'.format(posts.title),
+                                                                     url='{}{}'.format(os.environ.get("REDDIT_BASE_URL"), posts.permalink),
+                                                                     colour=colour)
+                        embed.set_author(name='New image post on {}'.format(posts.subreddit_name_prefixed),
+                                         url=posts.url)
                         embed.add_field(name='Author',
                                         value='{}'.format(posts.author),
                                         inline=True)
@@ -78,11 +80,12 @@ class RedditPosts(commands.Cog):
                         embed.set_footer(text='Ups: {}'.format(posts.ups))
                         await channel.send(embed=embed)
                     else:
-                        embed = discord.Embed(title='New image post on {}'.format(posts.subreddit_name_prefixed),
-                                              url=posts.url,
+                        print("A new image post was posted with id: {}".format(posts.id))
+                        embed = discord.Embed(title='{}'.format(posts.title),
+                                              url='{}{}'.format(os.environ.get("REDDIT_BASE_URL"), posts.permalink),
                                               colour=colour)
-                        embed.set_author(name='{}'.format(posts.title),
-                                         url='{}{}'.format(os.environ.get("REDDIT_BASE_URL"), posts.permalink))
+                        embed.set_author(name='New image post on {}'.format(posts.subreddit_name_prefixed),
+                                         url=posts.url)
                         #  embed.set_thumbnail(url='') <- if i want to use "compact mode" use thumbnail instead of
                         #  large pick
                         embed.add_field(name='Author',
