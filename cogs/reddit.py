f7c31b32fe19d90c4d2213f8b70c82a97235d055
Robert Ridal <ridal.robert@gmail.com>
2020-06-04 13:16:47 +0300

    removed is_closed

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -41,27 +41,44 @@ class RedditPosts(commands.Cog):
         channel = self.chika.get_channel(int(os.environ.get("DISCORD_CHANNEL_ID")))  # specific channel
         colour = discord.colour.Color.red()
 
-        while not self.chika.is_closed:
-            # Get the post
-            for posts in reddit.subreddit(os.environ.get("SUBREDDIT")).stream.submissions(pause_after=0):
-                if posts is None:
-                    # Wait 60 seconds for new submission
+        # while not self.chika.is_closed: I think this blocks the whole thing
+        # Get the post
+        for posts in reddit.subreddit(os.environ.get("SUBREDDIT")).stream.submissions(pause_after=0):
+            if posts is None:
+                # Wait 60 seconds for new submission
+                await asyncio.sleep(60)
+            elif posts.created_utc > current_date_in_seconds:
+                print("New post was found. With id: {} and created time was: {}".format(posts.id, time.strftime(
+                    '%Y-%m-%d %H:%M:%S', time.localtime(posts.created_utc))))
+                if posts.is_self == True and posts.is_reddit_media_domain == False:
+                    print("A new text post was posted with id: {}".format(posts.id))
+                    # if posts.over_18 == True:
+                    #     print("Content is over_18")
+                    embed = discord.Embed(title='{}'.format(posts.title),
+                                        url=posts.url,
+                                        # description='{}'.format(posts.selftext),
+                                        description='{}'.format((posts.selftext[:2045] + '...') if len(
+                                            posts.selftext) > 2048 else posts.selftext),
+                                        colour=colour)
+                    embed.set_author(name='New text post on {}'.format(posts.subreddit_name_prefixed),
+                                    url='{}'.format(posts.url))
+                    embed.add_field(name='Author',
+                                    value='{}'.format(posts.author),
+                                    inline=True)
+                    embed.add_field(name='NSFW',
+                                    value='{}'.format(posts.over_18),
+                                    inline=True)
+                    embed.set_footer(text='Ups: {}'.format(posts.ups))
+                    await channel.send(embed=embed)
                     await asyncio.sleep(60)
-                elif posts.created_utc > current_date_in_seconds:
-                    print("New post was found. With id: {} and created time was: {}".format(posts.id, time.strftime(
-                        '%Y-%m-%d %H:%M:%S', time.localtime(posts.created_utc))))
-                    if posts.is_self == True and posts.is_reddit_media_domain == False:
-                        print("A new text post was posted with id: {}".format(posts.id))
-                        # if posts.over_18 == True:
-                        #     print("Content is over_18")
+                if posts.is_self == False and posts.is_reddit_media_domain == True:
+                    if posts.over_18 == True:
+                        print("A new K-18 image post was posted with id: {}".format(posts.id))
                         embed = discord.Embed(title='{}'.format(posts.title),
-                                            url=posts.url,
-                                            # description='{}'.format(posts.selftext),
-                                            description='{}'.format((posts.selftext[:2045] + '...') if len(
-                                                posts.selftext) > 2048 else posts.selftext),
+                                            url='{}{}'.format(os.environ.get("REDDIT_BASE_URL"), posts.permalink),
                                             colour=colour)
-                        embed.set_author(name='New text post on {}'.format(posts.subreddit_name_prefixed),
-                                        url='{}'.format(posts.url))
+                        embed.set_author(name='New image post on {}'.format(posts.subreddit_name_prefixed),
+                                        url=posts.url)
                         embed.add_field(name='Author',
                                         value='{}'.format(posts.author),
                                         inline=True)
@@ -71,43 +88,26 @@ class RedditPosts(commands.Cog):
                         embed.set_footer(text='Ups: {}'.format(posts.ups))
                         await channel.send(embed=embed)
                         await asyncio.sleep(60)
-                    if posts.is_self == False and posts.is_reddit_media_domain == True:
-                        if posts.over_18 == True:
-                            print("A new K-18 image post was posted with id: {}".format(posts.id))
-                            embed = discord.Embed(title='{}'.format(posts.title),
-                                                url='{}{}'.format(os.environ.get("REDDIT_BASE_URL"), posts.permalink),
-                                                colour=colour)
-                            embed.set_author(name='New image post on {}'.format(posts.subreddit_name_prefixed),
-                                            url=posts.url)
-                            embed.add_field(name='Author',
-                                            value='{}'.format(posts.author),
-                                            inline=True)
-                            embed.add_field(name='NSFW',
-                                            value='{}'.format(posts.over_18),
-                                            inline=True)
-                            embed.set_footer(text='Ups: {}'.format(posts.ups))
-                            await channel.send(embed=embed)
-                            await asyncio.sleep(60)
-                        else:
-                            print("A new image post was posted with id: {}".format(posts.id))
-                            embed = discord.Embed(title='{}'.format(posts.title),
-                                                url='{}{}'.format(os.environ.get("REDDIT_BASE_URL"), posts.permalink),
-                                                colour=colour)
-                            embed.set_author(name='New image post on {}'.format(posts.subreddit_name_prefixed),
-                                            url=posts.url)
-                            #  embed.set_thumbnail(url='') <- if i want to use "compact mode" use thumbnail instead of
-                            #  large pick
-                            embed.add_field(name='Author',
-                                            value='{}'.format(posts.author),
-                                            inline=True)
-                            embed.add_field(name='NSFW',
-                                            value='{}'.format(posts.over_18),
-                                            inline=True)
-                            embed.set_image(url='{}'.format(posts.url))
-                            embed.set_footer(text='Ups: {}'.format(posts.ups))
-                            await channel.send(embed=embed)
-                            await asyncio.sleep(60)
-                # await asyncio.sleep(60) # This didnt work and still sends multiple of the same post
+                    else:
+                        print("A new image post was posted with id: {}".format(posts.id))
+                        embed = discord.Embed(title='{}'.format(posts.title),
+                                            url='{}{}'.format(os.environ.get("REDDIT_BASE_URL"), posts.permalink),
+                                            colour=colour)
+                        embed.set_author(name='New image post on {}'.format(posts.subreddit_name_prefixed),
+                                        url=posts.url)
+                        #  embed.set_thumbnail(url='') <- if i want to use "compact mode" use thumbnail instead of
+                        #  large pick
+                        embed.add_field(name='Author',
+                                        value='{}'.format(posts.author),
+                                        inline=True)
+                        embed.add_field(name='NSFW',
+                                        value='{}'.format(posts.over_18),
+                                        inline=True)
+                        embed.set_image(url='{}'.format(posts.url))
+                        embed.set_footer(text='Ups: {}'.format(posts.ups))
+                        await channel.send(embed=embed)
+                        await asyncio.sleep(60)
+            # await asyncio.sleep(60) # This didnt work and still sends multiple of the same post
 
 def setup(chika):
     chika.add_cog(RedditPosts(chika))
